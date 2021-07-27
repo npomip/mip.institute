0d465e2342b8868dab1baf51c86281b2fc3e9901
DrunkestManEvar <fullextentofsuperiority@mail.ru>
2021-07-27 14:42:57 +0300

    Added Reviews section

--- components/layout/TwoColumns.tsx
+++ components/layout/TwoColumns.tsx
@@ -1,7 +1,12 @@
 import stls from '@/styles/components/layout/TwoColumns.module.sass'
+import classNames from 'classnames'
 
-const TwoColumns = ({ children }) => {
-  return <div className={stls.container}>{children}</div>
+const TwoColumns = ({ slider = false, children }) => {
+  return (
+    <div className={classNames(stls.container, { [stls.scrollable]: slider })}>
+      {children}
+    </div>
+  )
 }
 
 export default TwoColumns
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Hero from '@/components/sections/Hero'
+import Reviews from '@/components/sections/Reviews'
 
 const Home = ({ programs }) => {
   return (
     <>
-      <Hero />
+      {/* <Hero /> */}
+      <Reviews />
     </>
   )
 }

58e92796573be0089bc1b7e20b93fba2499d8bc0
novailoveyou <novailoveyoux@gmail.com>
2021-07-27 15:31:00 +0300

    improved Hero section & added ProsList component

--- components/icons/index.ts
+++ components/icons/index.ts
@@ -1,7 +1,15 @@
+import IconAtom from '@/components/icons/IconAtom'
+import IconCircleCheck from '@/components/icons/IconCircleCheck'
 import IconHumburger from '@/components/icons/IconHumburger'
 import IconLogo from '@/components/icons/IconLogo'
 import IconPhone from '@/components/icons/IconPhone'
 import IconMoreThan from '@/components/icons/IconMoreThan'
-import IconAtom from '@/components/icons/IconAtom'
 
-export { IconHumburger, IconLogo, IconPhone, IconMoreThan, IconAtom }
+export {
+  IconAtom,
+  IconCircleCheck,
+  IconHumburger,
+  IconLogo,
+  IconPhone,
+  IconMoreThan
+}
--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -1,6 +1,7 @@
 import stls from '@/styles/components/sections/Hero.module.sass'
 import Wrapper from '@/components/layout/Wrapper'
 import TwoColumns from '@/components/layout/TwoColumns'
+import ProsList from '@/components/general/ProsList'
 import { IconAtom } from '@/components/icons'
 import { ImgLadyStudying } from '@/components/imgs'
 import { BtnAlpha, BtnBeta } from '@/components/btns'
@@ -21,6 +22,7 @@ const Hero = () => {
   }
   return (
     <section className={stls.container}>
+      <div className={stls.bg}></div>
       <Wrapper>
         <div className={stls.icon} onClick={e => hiddenGem(e)}>
           <IconAtom />
@@ -29,7 +31,7 @@ const Hero = () => {
           <IconAtom light large />
         </div>
         <TwoColumns>
-          <div>
+          <div className={stls.left}>
             <h1 className={stls.title}>Онлайн-институт психологии</h1>
             <h2 className={stls.desc}>
               Освойте востребованную профессию психолога или повысьте
@@ -42,8 +44,15 @@ const Hero = () => {
               </TwoColumns>
             </div>
           </div>
-          <div className={stls.img}>
-            <ImgLadyStudying />
+          <div className={stls.right}>
+            <TwoColumns>
+              <div className={stls.img}>
+                <ImgLadyStudying />
+              </div>
+              <div className={stls.prosList}>
+                <ProsList />
+              </div>
+            </TwoColumns>
           </div>
         </TwoColumns>
       </Wrapper>

c4071ab3fc961e844782da651be60f5bfc75dd61
Novailoveyou <novailoveyou@gmail.com>
2021-07-29 18:19:29 +0300

    improved files structure: courses data now in ./data

--- components/programs/Courses.tsx
+++ components/programs/Courses.tsx
@@ -1,67 +1,9 @@
 import stls from '@/styles/components/programs/Courses.module.sass'
 import CardCourse from '@/components/cards/CardCourse'
 import { BtnDelta } from '@/components/btns'
+import courses from '@/data/programs/courses'
 
 const Courses = () => {
-  const courses = [
-    {
-      title: 'Психоанализ и психологическое консультирование',
-      type: 'course',
-      typeLabel: 'Курс',
-      dur: { months: 4 },
-      durLabel: '4 месяца'
-    },
-    {
-      title: 'Название обучающей программы',
-      type: 'course',
-      typeLabel: 'Курс',
-      dur: { months: 4 },
-      durLabel: '4 месяца'
-    },
-    {
-      title: 'Кризисный психолог',
-      type: 'course',
-      typeLabel: 'Курс',
-      dur: { months: 4 },
-      durLabel: '4 месяца'
-    },
-    {
-      title: 'Психоанализ и психологическое консультирование',
-      type: 'course',
-      typeLabel: 'Курс',
-      dur: { months: 4 },
-      durLabel: '4 месяца'
-    },
-    {
-      title: 'Психоанализ и психологическое консультированиее',
-      type: 'course',
-      typeLabel: 'Курс',
-      dur: { months: 4 },
-      durLabel: '4 месяца'
-    },
-    {
-      title: 'Название обучающие программы',
-      type: 'course',
-      typeLabel: 'Курс',
-      dur: { months: 4 },
-      durLabel: '4 месяца'
-    },
-    {
-      title: 'Кризисный психолог',
-      type: 'course',
-      typeLabel: 'Курс',
-      dur: { months: 4 },
-      durLabel: '4 месяца'
-    },
-    {
-      title: 'Психоанализ и психологическое консультирование',
-      type: 'course',
-      typeLabel: 'Курс',
-      dur: { months: 4 },
-      durLabel: '4 месяца'
-    }
-  ]
-
   return (
     <div className={stls.courses}>
       <hgroup>

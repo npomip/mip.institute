690ec0c1fc407a2fce94795014f4171037ed24ac
novailoveyou <novailoveyoux@gmail.com>
2021-08-22 22:57:07 +0300

    test commit

--- components/program/ProgramModules.tsx
+++ components/program/ProgramModules.tsx
@@ -3,7 +3,7 @@ import ProgramModule from '@/components/program/ProgramModule'
 import ProgramContext from '@/context/program/programContext'
 import { useContext } from 'react'
 import { getListItemsInnerHtml } from '@/helpers/index'
-
+//
 const ProgramModules = () => {
   const {
     program: { shortContents }

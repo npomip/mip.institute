50a39757083a00901a9206299157eecd7d7b6892
novailoveyou <novailoveyoux@gmail.com>
2021-08-23 01:26:17 +0300

    made helper function take less lines

--- helpers/filterProgramsByType.ts
+++ helpers/filterProgramsByType.ts
@@ -15,12 +15,12 @@ const filterProgramsByType = ({
       "You've passed in falsy prop 'type' somewhere or haven't specified it. Prop 'type' can't be falsy and has to be a string with either 'course' or 'profession' as a value"
     )
 
-  const output =
+  return (
     programs &&
     programs.filter(
       program => program.type && program.type.toLowerCase() === type
     )
-  return output
+  )
 }
 
 export default filterProgramsByType

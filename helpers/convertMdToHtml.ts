f92063b10f82a7e6f4d8ab34b6724e00626de1ef
novailoveyou <novailoveyoux@gmail.com>
2021-08-23 01:27:20 +0300

    another deployment fix error

--- helpers/convertMdToHtml.ts
+++ helpers/convertMdToHtml.ts
@@ -3,16 +3,18 @@ import marked from 'marked'
 const convertMdToHtml = ({ arr = [], params = null }) => {
   let output = null
   if (arr && arr.length === 0) {
-    params.forEach(param => {
-      output = marked(param)
-    })
+    params &&
+      params.forEach(param => {
+        output = marked(param)
+      })
   } else {
     output =
       arr &&
       arr.map(item => {
-        params.forEach(param => {
-          item[param] = marked(item[param])
-        })
+        params &&
+          params.forEach(param => {
+            item[param] = marked(item[param])
+          })
         return item
       })
   }

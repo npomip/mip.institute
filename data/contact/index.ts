52e4bb76761889492ac2c0aa2acb029dbeec2fc8
novailoveyou <novailoveyoux@gmail.com>
2021-10-22 10:13:59 +0300

    improved on contacts data

--- data/contact/index.ts
+++ data/contact/index.ts
@@ -1,4 +1,6 @@
 import number from '@/data/contact/number'
 import numberAlt from '@/data/contact/numberAlt'
+import numberAlmaty from '@/data/contact/numberAlmaty'
+import numberAlmatyAlt from '@/data/contact/numberAlmatyAlt'
 
-export { number, numberAlt }
+export { number, numberAlt, numberAlmaty, numberAlmatyAlt }
--- data/location/index.ts
+++ data/location/index.ts
@@ -1,6 +1,6 @@
 import city from '@/data/location/city'
-import cityAlmaty from '@/data/location/cityAlmaty'
 import street from '@/data/location/street'
+import cityAlmaty from '@/data/location/cityAlmaty'
 import streetAlmaty from '@/data/location/streetAlmaty'
 
 export { city, street, cityAlmaty, streetAlmaty }

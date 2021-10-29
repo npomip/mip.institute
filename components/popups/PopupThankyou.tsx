246ee808e8159fd3b32acbc03986831bf1194636
novailoveyou <novailoveyoux@gmail.com>
2021-10-29 13:09:46 +0300

    added lead event to popupthankyou

--- components/popups/PopupThankyou.tsx
+++ components/popups/PopupThankyou.tsx
@@ -1,7 +1,37 @@
 import stls from '@/styles/components/popups/PopupThankyou.module.sass'
+import { useEffect, useContext } from 'react'
+import ProgramContext from '@/context/program/programContext'
+import TagManager from 'react-gtm-module'
 import { BtnClose } from '@/components/btns'
+import { v4 as uuidv4 } from 'uuid'
 
 const PopupThankyou = ({ close }) => {
+  const { program } = useContext(ProgramContext)
+
+  useEffect(() => {
+    const tagManagerArgs = {
+      dataLayer: {
+        event: 'lead',
+        ecommerce: {
+          add: {
+            actionField: {
+              id: uuidv4()
+            },
+            products: [
+              {
+                id: (program && program._id) || (program && program.id) || null,
+                name: (program && program.title) || null,
+                price: (program && program.price) || null,
+                type: (program && program.typeLabel) || null
+              }
+            ]
+          }
+        }
+      },
+      dataLayerName: 'dataLayer'
+    }
+    TagManager.dataLayer(tagManagerArgs)
+  }, [])
   return (
     <div className={stls.container}>
       <div className={stls.close}>

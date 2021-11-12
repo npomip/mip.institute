de1f9e70f62317feea7a5720765c87afbd737e79
Novailoveyou <novailoveyoux@gmail.com>
2021-11-12 14:32:20 +0300

    StickyBottom for desktop is done

--- components/icons/IconCloseCircle.tsx
+++ components/icons/IconCloseCircle.tsx
@@ -1,6 +1,10 @@
 import stls from '@/styles/components/icons/IconCloseCircle.module.sass'
 
-const IconCloseCircle = () => {
+type TypeIconCloseCircle = {
+  blackCross?: boolean
+}
+
+const IconCloseCircle = ({ blackCross = false }) => {
   return (
     <div className={stls.container}>
       <svg viewBox='0 0 32 32' fill='none' xmlns='http://www.w3.org/2000/svg'>
@@ -8,14 +12,14 @@ const IconCloseCircle = () => {
         <circle cx='16' cy='16' r='16' fill='#F5F5F5' />
         <path
           d='M11.2002 11.2L20.8002 20.8'
-          stroke='#D9D9D9'
+          stroke={blackCross ? '#000' : '#D9D9D9'}
           strokeWidth='2'
           strokeLinecap='round'
           strokeLinejoin='round'
         />
         <path
           d='M20.7998 11.2L11.1998 20.8'
-          stroke='#D9D9D9'
+          stroke={blackCross ? '#000' : '#D9D9D9'}
           strokeWidth='2'
           strokeLinecap='round'
           strokeLinejoin='round'
--- components/layout/StickyBottom.tsx
+++ components/layout/StickyBottom.tsx
@@ -1,12 +1,41 @@
 import stls from '@/styles/components/layout/StickyBottom.module.sass'
+import { useRouter } from 'next/router'
+import { useState, useEffect } from 'react'
 import { discount, until } from '@/data/price'
+import { routePayment } from '@/data/routes'
 import Wrapper from '@/components/layout/Wrapper'
 import IconWavyShape from '@/components/icons/IconWavyShape'
 import PopupTrigger from '@/components/general/PopupTrigger'
+import { IconCloseCircle } from '@/components/icons'
+import classNames from 'classnames'
 
 const StickyBottom = () => {
+  const router = useRouter()
+
+  const [isShown, setIsShown] = useState(false)
+  const [isClosed, setIsClosed] = useState(false)
+
+  useEffect(() => {
+    if (router.asPath !== routePayment) {
+      document.addEventListener('scroll', () => {
+        const scrollHeight = document.body.scrollHeight
+        const pageYOffset = window.pageYOffset
+        pageYOffset > (scrollHeight * 10) / 100 &&
+        pageYOffset + window.innerHeight < (scrollHeight * 90) / 100 &&
+        !isClosed
+          ? setIsShown(true)
+          : setIsShown(false)
+      })
+    }
+  }, [router, setIsShown, isClosed])
+
   return (
-    <div className={stls.container}>
+    <div
+      className={classNames({
+        [stls.container]: true,
+        [stls.isShown]: isShown,
+        [stls.isClosed]: isClosed
+      })}>
       <Wrapper>
         <div className={stls.icon}>
           <IconWavyShape />
@@ -17,9 +46,14 @@ const StickyBottom = () => {
         </p>
         <div className={stls.btns}>
           <PopupTrigger btn='theta' cta='learnAboutUs' />
-          <PopupTrigger btn='alpha' cta='consultMe' />
+          <div className={stls.btn2}>
+            <PopupTrigger btn='alpha' cta='consultMe' />
+          </div>
         </div>
       </Wrapper>
+      <button className={stls.close} onClick={() => setIsClosed(true)}>
+        <IconCloseCircle blackCross />
+      </button>
     </div>
   )
 }

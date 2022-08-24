e791bc30f1a8ff52603cf8c51f042ca0f742af31
novailoveyou <novailoveyoux@gmail.com>
2022-08-24 12:25:41 +0300

    added legal doc

--- data/docs/constituent/dataDocsConstituentRight.ts
+++ data/docs/constituent/dataDocsConstituentRight.ts
@@ -5,6 +5,10 @@ const dataDocsConstituentRight: TypeDataDocs = [
   {
     href: `${routes.front.docsConstituent}/ustav-obrazovatelnoj-organizacii-nano-mip.pdf`,
     val: 'Устав образовательной организации НАНО МИП'
+  },
+  {
+    href: `${routes.front.docsConstituent}/vypiska-licenzii-nano-mip-041363.pdf`,
+    val: 'Выписка лицензии НАНО МИП №041363'
   }
 ]
 

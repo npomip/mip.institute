7fa1e72b311ad84d03898c2b447f84240527a985
Evgenie <nestroykozni@gmail.com>
2023-03-15 12:36:35 +0300

    fix: calculateClosestAdmission

--- helpers/calculateClosestAdmission.ts
+++ helpers/calculateClosestAdmission.ts
@@ -1,36 +1,23 @@
-import { getRuMonths } from '@/helpers/index'
+const setLastDayOfMonth = (currentDate: Date) => {
+  currentDate.setMonth(currentDate.getMonth() + 1)
+  currentDate.setDate(0)
+}
+
+const setNextDay = (currentDate: Date, currentDay: number) =>
+  currentDay <= 20
+    ? currentDate.setDate(currentDay <= 10 ? 10 : 20)
+    : setLastDayOfMonth(currentDate)
 
 const calculateClosestAdmission = () => {
   const currentDate = new Date()
   const currentDay = currentDate.getDate()
-  const currentMonth = currentDate.getMonth()
-  const months = getRuMonths()
 
-  // const currendDayCondition = currentDay >= 5 && currentDay < 20
-  const currendDayCondition = currentDay < 20
+  setNextDay(currentDate, currentDay)
 
-  // const day = currendDayCondition ? '20' : '5'
-  const day = currentDay >= 5 && currendDayCondition ? '20' : '5'
-  // {currentDay < 5 ? '5' : currentDay >= 5 && currendDayCondition ? '20' : '5'}{' '}
-  // const lastDayOfCurMonth = new Date(
-  //   currentDate.getFullYear(),
-  //   currentDate.getMonth() + 1,
-  //   0
-  // ).getDate()
-  // const until =
-  // currendDayCondition ? `до 20 ${output}` : `до ${lastDayOfCurMonth} ${output}`
-
-  let output
-  if (currendDayCondition) {
-    output = `${day} ${months[currentMonth]}`
-  } else {
-    currentMonth === 11
-      ? (output = `${day} ${months[0]}`)
-      : (output = `${day} ${months[currentMonth + 1]}`)
-  }
-
-  return '10 марта'
-  return output
+  return currentDate.toLocaleString('ru-RU', {
+    day: 'numeric',
+    month: 'long'
+  })
 }
 
 export default calculateClosestAdmission

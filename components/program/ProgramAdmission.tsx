36377bd39e6cdd470681debf9f0e18130f0f37d4
Ivan Kolpakov <vanjaklp95@gmail.com>
2023-06-21 11:44:57 +0300

    date

--- components/program/ProgramAdmission.tsx
+++ components/program/ProgramAdmission.tsx
@@ -2,7 +2,7 @@ import { calculateClosestAdmission, setDateOfEnrollment } from '@/helpers/index'
 
 const ProgramAdmission = () => {
   // return <>{calculateClosestAdmission()}</>
-  return <>{setDateOfEnrollment(20,6)}</>
+  return <>{setDateOfEnrollment(30,6)}</>
 }
 
 export default ProgramAdmission

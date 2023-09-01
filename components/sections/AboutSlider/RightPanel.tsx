110257d3c2aee385769546ee6153b825d2de265f
Ivan Kolpakov <vanjaklp95@gmail.com>
2023-09-01 15:53:11 +0300

    slides sizes

--- components/sections/AboutSlider/RightPanel.tsx
+++ components/sections/AboutSlider/RightPanel.tsx
@@ -2,17 +2,20 @@ import stls from '@/styles/components/sections/AboutSlider/RightPanel.module.sas
 import classNames from 'classnames'
 import Image from 'next/image'
 
-
 const RightPanel = ({ imageUrl, slideDirection }) => {
   return (
     // <div className={classNames({
-      // [stls.rightPanel]: true, 
-      // [stls.slideDirection]: slideDirection})}></div>
+    // [stls.rightPanel]: true,
+    // [stls.slideDirection]: slideDirection})}></div>
     <div className={classNames(stls['right-panel'], stls[`${slideDirection}`])}>
       <div className={stls.img}>
-        <Image src={imageUrl} alt={'divan'} />
+        <Image
+        className={stls.innerImg} 
+        // layout="responsive"
+        // width={580}
+        // height={575}
+        src={imageUrl} alt={'divan'} />
       </div>
-      
     </div>
   )
 }

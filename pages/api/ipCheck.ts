20c193b48b8551f9bbf1f544e3098be9fc7bf71d
Ivan Kolpakov <vanjaklp95@gmail.com>
2023-10-13 13:04:15 +0300

    denied ips added to form

---
+++ pages/api/ipCheck.ts
@@ -0,0 +1,24 @@
+import unwantedIps from '@/helpers/unwantedIps';
+
+export default async function handler(req, res) {
+
+  console.log(req.connection)
+  const ip =
+    req.headers['x-forwarded-for'] ||
+    // req.socket.remoteAddress ||
+    req.connection.remoteAddress
+  console.log(ip)
+
+  try {
+    if (unwantedIps.includes(ip)) {
+      console.log('IP-адрес в списке нежелательных. Сообщение не отправлено.');
+      return res.status(403).json({ status: 403, msg: 'Access denied' });
+    } else {
+      return res.status(200).json({ status: 200, msg: 'Access permited' });
+    }
+    
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ success: false });
+  }
+}

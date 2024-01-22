74faf8c63b806b5f365fbc8861ac68957b34450f
Ivan Kolpakov <vanjaklp95@gmail.com>
2024-01-22 15:18:04 +0300

    realted programs remade

--- components/articles/ArticleBlogRelatedPrograms.tsx
+++ components/articles/ArticleBlogRelatedPrograms.tsx
@@ -1,66 +1,47 @@
-import stls from '@/styles/components/articles/ArticleBlogRelatedPrograms.module.sass'
-import Wrapper from '@/components/layout/Wrapper'
-import classNames from 'classnames'
-import marked from 'marked'
-import parse from 'html-react-parser'
-import RelatedProgramsList from './RelatedProgramsList'
+import ArticleBlogOneProgram from './ArticleBlogOneProgram'
+import ArticleBlogMoreThenOneProgram from './ArticleBlogMoreThenOneProgram'
 
-type ArticleBlogRelatedProgramsType = {
+export type ArticleBlogRelatedProgramsType = {
   props: {
+    title: string
+    borderColor?: {
+      code?: string
+    }
+    textItem: {
+      id
+      text :string
+    }[]
+    programs: {
+      id: string
+      type: string
+      slug: string
+      studyFieldSlug: string
       title: string
-      borderColor?: {
-        code?: string 
+      
+      heroPicture: {
+        height: string
+        url: string
+        width: string
       }
-      programs: {
-        id: string
-        type: string
-        slug: string
-        studyFieldSlug: string
-        title: string
-        heroPicture: {
-          height: string
-          url: string
-          width: string
-        }
-      }[]
+    }[]
   }
-  
 }
 
-const ArticleBlogRelatedPrograms = ({props} : ArticleBlogRelatedProgramsType) => {
-  // const text = props.text;
-  // console.log(props)
-  
+const ArticleBlogRelatedPrograms = ({
+  props
+}: ArticleBlogRelatedProgramsType) => {
 
-  const renderer = new marked.Renderer();
-  renderer.em = (text) => {
-    return `<span  style="color: ${props?.borderColor?.code}">${text}</span>`;
-  };
-  renderer.paragraph = (text) => {
-    return `<p className=${stls.title}>${text}</p>`
-  }
-  marked.setOptions({ renderer });
-
-  const title = marked(props?.title)
   const programs = props?.programs
 
+
   return (
-      <div 
-      // style={{border: `1px solid ${props.borderColor.code}`}} 
-      className={stls.relatedPrograms}>
-        <div className={stls.innerBox}>
-          <div className={stls.relatedProgramsTitle}>
-            {parse(title)}
-          </div>
-          <p>Курсы:</p>
-          
-          <div className={stls.programs}>
-            {programs.map((program) => (
-              <RelatedProgramsList program={program} key={program.id} />
-            ))}
-          </div>
-        </div>
-      </div>
+    <>
+    {programs.length === 1 ? (
+      <ArticleBlogOneProgram props={props} />
+    ) : (
+      <ArticleBlogMoreThenOneProgram props={props} />
+    )}
+    </>
   )
 }
 

295ed8b3e921038bc261212464525483d3596b95
Ivan Kolpakov <vanjaklp95@gmail.com>
2024-01-29 14:18:50 +0300

    Journal breadcrumbs, date in slug, time of read added

--- components/general/Breadcrumbs.tsx
+++ components/general/Breadcrumbs.tsx
@@ -1,23 +1,29 @@
 import stls from '@/styles/components/general/Breadcrumbs.module.sass'
 import Link from 'next/link'
+import classNames from 'classnames'
 
-const Breadcrumbs = ({breadcrumbs}) => {
-
+const Breadcrumbs = ({ breadcrumbs, isJournal = false }) => {
   return (
     <div className={stls.container}>
-      <ul className={stls.linkList}>
+      <ul
+        className={classNames({
+          [stls.linkList]: true,
+          [stls.isJournal]: isJournal
+        })}
+        // className={stls.linkList}
+      >
         <li>
-          <Link href='/'>
-          Главная
-          </Link>
+          <Link href='/'>Главная</Link>
         </li>
         {breadcrumbs.map(el => (
-          <li  key={el.path}>
+          <li
+            // className={classNames({ [stls.isJournalLi]: isJournal })}
+            key={el.path}>
             <span className={stls.triangle}></span>
             <Link className={stls.links} href={el.path}>
-            {el.label}
+              {el.label}
             </Link>
-            </li>
+          </li>
         ))}
       </ul>
     </div>

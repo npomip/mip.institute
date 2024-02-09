8c956ffc7d849229304c3769768d7ffa2776a9b5
Mikhail <molyanenko@phoenixit.ru>
2024-02-09 17:03:41 +0300

    замена buttonToTop

--- components/sections/ButtonToTop.tsx
+++ components/sections/ButtonToTop.tsx
@@ -1,37 +1,37 @@
+import { IconArrowUpRound } from '@/components/icons'
 import stls from '@/styles/components/sections/ButtonToTop.module.sass'
-import { IconArrowRight } from '@/components/icons'
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
 
 const ButtonToTop = () => {
-  const [isVisible, setIsVisible] = useState(false);
+  const [isVisible, setIsVisible] = useState(false)
 
   const toggleVisibility = () => {
     if (window?.scrollY > 300) {
-      setIsVisible(true);
+      setIsVisible(true)
     } else {
-      setIsVisible(false);
+      setIsVisible(false)
     }
-  };
+  }
+
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
-      behavior: 'smooth',
-    });
-  };
+      behavior: 'smooth'
+    })
+  }
 
   useEffect(() => {
-    window.addEventListener('scroll', toggleVisibility);
+    window.addEventListener('scroll', toggleVisibility)
     return () => {
-      window.removeEventListener('scroll', toggleVisibility);
-    };
-  }, []);
+      window.removeEventListener('scroll', toggleVisibility)
+    }
+  }, [])
 
   return (
-    <div onClick={scrollToTop} className={`${stls.scrollToTop} ${isVisible ? stls.show : ''}`}>
-      <div className={stls.icon}>
-
-        <IconArrowRight />
-      </div>
+    <div
+      onClick={scrollToTop}
+      className={`${stls.scrollToTop} ${isVisible ? stls.show : ''}`}>
+      <IconArrowUpRound />
     </div>
   )
 }

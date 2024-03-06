c98cc66c2b56df74663310280c2e698119897dda
Mikhail <molyanenko@phoenixit.ru>
2024-03-06 11:06:09 +0300

    directions

--- components/sections/ChooseProgram/ProgramList.tsx
+++ components/sections/ChooseProgram/ProgramList.tsx
@@ -1,87 +1,70 @@
 import routes from '@/config/routes'
-import { ContextStaticProps } from '@/context/index'
-import filterProgramsByStudyField from '@/helpers/filterProgramsByStudyField'
 import styles from '@/styles/components/sections/ChooseProgram/ProgramList.module.sass'
-import { useContext, useState } from 'react'
+import { useState } from 'react'
 import Program from './Program'
+import Link from 'next/link'
 
-export default function ProgramList({ ofType = null, currentType = null }) {
-  const [hoveredSlug, setHoveredSlug] = useState(null)
+const studyFieldsProfessions = [
+  { label: 'Клиническая психология', slug: 'klinicheskaya-psihologiya' },
+  {
+    label: 'Диетология и нутрициология',
+    slug: 'dietologiya-i-nutriciologiya'
+  },
+  { label: 'Консультирование', slug: 'konsultirovanie' },
+  { label: 'Психотерапия', slug: 'psihoterapiya' },
+  {
+    label: 'Организационная психология',
+    slug: 'organizacionnaya-psihologiya'
+  },
+  { label: 'Общая психология', slug: 'obshaya-psihologiya' },
+  { label: 'Детская психология', slug: 'detskaya-psihologiya' }
+]
 
-  const studyFields = [
-    
-    { label: 'Общая психология', slug: 'obshaya-psihologiya' },
-    { label: 'Клиническая психология', slug: 'klinicheskaya-psihologiya' },
-    {
-      label: 'Организационная психология',
-      slug: 'organizacionnaya-psihologiya'
-    },
-    {
-      label: 'Диетология и нутрициология',
-      slug: 'dietologiya-i-nutriciologiya'
-    },
-    { label: 'Консультирование', slug: 'konsultirovanie' },
-    { label: 'Детская психология', slug: 'detskaya-psihologiya' },
-    { label: 'Психотерапия', slug: 'psihoterapiya' },
-  ]
+const studyFieldsCourses = [
+  { label: 'Детская психология', slug: 'detskaya-psihologiya' },
+  { label: 'Общая психология', slug: 'obshaya-psihologiya' },
+  {
+    label: 'Организационная психология',
+    slug: 'organizacionnaya-psihologiya'
+  },
+  { label: 'Консультирование', slug: 'konsultirovanie' },
+  { label: 'Психотерапия', slug: 'psihoterapiya' }
+]
 
-  const studyFieldsProfessions = [
-    { label: 'Консультирование', slug: 'konsultirovanie' },
-    { label: 'Детская психология', slug: 'detskaya-psihologiya' },
-    { label: 'Психотерапия', slug: 'psihoterapiya' },
-    { label: 'Общая психология', slug: 'obshaya-psihologiya' },
-    { label: 'Клиническая психология', slug: 'klinicheskaya-psihologiya' },
-    {
-      label: 'Организационная психология',
-      slug: 'organizacionnaya-psihologiya'
-    },
-    {
-      label: 'Диетология и нутрициология',
-      slug: 'dietologiya-i-nutriciologiya'
-    },
-    
-  ]
+type Props = {
+  ofType: string
+  currentType?: string
+}
 
-  const studyFieldsCourses = [
-    { label: 'Консультирование', slug: 'konsultirovanie' },
-    { label: 'Детская психология', slug: 'detskaya-psihologiya' },
-    { label: 'Общая психология', slug: 'obshaya-psihologiya' },
-    { label: 'Психотерапия', slug: 'psihoterapiya' },
-    
-    {
-      label: 'Организационная психология',
-      slug: 'organizacionnaya-psihologiya'
-    }
-  ]
-
-  const list =
-    ofType === 'course'
-      ? studyFieldsCourses
-      : ofType === 'profession'
-      ? studyFieldsProfessions
-      : studyFields
+export default function ProgramList({ ofType, currentType }: Props) {
+  const list = ofType === 'course' ? studyFieldsCourses : studyFieldsProfessions
 
   const [openListIndex, setOpenListIndex] = useState(-1)
 
   return (
     <div className={styles.program}>
+      {ofType === 'course' && (
+        <Link href={routes.front.courses}>
+          <a className={styles.all} style={{ margin: 0 }}>
+            Все направления
+          </a>
+        </Link>
+      )}
       {list.map(({ label, slug }, index) => (
         <Program
           ofType={ofType}
-          setHoveredSlug={setHoveredSlug}
           index={index}
           slug={slug}
           key={`${slug}-${index}`}
           label={label}
           href={`${
-            currentType === 'course'
+            ofType === 'course'
               ? routes.front.courses
-              : currentType === 'profession'
-              ? routes.front.professions
               : routes.front.professions
           }/${slug}`}
           openListIndex={openListIndex} // Передаем состояние открытого списка
           setOpenListIndex={setOpenListIndex}
+          showIcon={ofType !== 'course'}
         />
       ))}
     </div>
--- components/sections/DirectionsSelector.tsx
+++ components/sections/DirectionsSelector.tsx
@@ -1,36 +1,37 @@
-import stls from '@/styles/components/sections/DirectionsSelector.module.sass';
-import { Fragment, useState } from 'react';
-import routes from '@/config/routes';
-import classNames from 'classnames';
-import { useMediaQuery } from 'react-responsive';
-import ProgramList from './ChooseProgram/ProgramList';
+import stls from '@/styles/components/sections/DirectionsSelector.module.sass'
+import { Dispatch, FC, Fragment, SetStateAction, useState } from 'react'
+import routes from '@/config/routes'
+import classNames from 'classnames'
+import { useMediaQuery } from 'react-responsive'
+import ProgramList from './ChooseProgram/ProgramList'
 
-const DirectionsSelector = ({ currentType = null, setCurrentType }) => {
+type Props = {
+  currentType?: string
+  setCurrentType?: Dispatch<SetStateAction<string>>
+}
+const DirectionsSelector: FC<Props> = ({
+  currentType = null,
+  setCurrentType
+}) => {
   const list = [
     {
       id: 1,
-      label: 'Дополнительное образование',
-      href: routes.front.programs,
-      programType: 'null',
+      label: 'Профессиональная переподготовка',
+      href: routes.front.professions,
+      programType: 'profession'
     },
     {
       id: 2,
-      label: 'Профессиональная переподготовка',
-      href: routes.front.professions,
-      programType: 'profession',
-    },
-    {
-      id: 3,
       label: 'Повышение квалификации',
       href: routes.front.courses,
-      programType: 'course',
-    },
-  ];
+      programType: 'course'
+    }
+  ]
 
-  const isMobileLayout = useMediaQuery({ query: '(max-width: 768px)' });
+  const isMobileLayout = useMediaQuery({ query: '(max-width: 768px)' })
 
   // Состояние для активного элемента
-  const [activeItem, setActiveItem] = useState(2);
+  const [activeItem, setActiveItem] = useState(1)
 
   return (
     <div className={stls.container}>
@@ -39,7 +40,7 @@ const DirectionsSelector = ({ currentType = null, setCurrentType }) => {
           <div
             className={classNames({
               [stls.studyField]: true,
-              [stls.active]: currentType === programType,
+              [stls.active]: currentType === programType
             })}
             onMouseEnter={
               !isMobileLayout ? () => setCurrentType(programType) : undefined
@@ -47,12 +48,12 @@ const DirectionsSelector = ({ currentType = null, setCurrentType }) => {
             onClick={() => {
               if (activeItem === id) {
                 // Если элемент уже активен, снимаем активность
-                setActiveItem(null);
-                setCurrentType(null);
+                setActiveItem(null)
+                setCurrentType(null)
               } else {
                 // Иначе, активируем элемент
-                setActiveItem(id);
-                setCurrentType(programType);
+                setActiveItem(id)
+                setCurrentType(programType)
               }
             }}>
             <p className={stls.mainFields}>{label}</p>
@@ -60,13 +61,22 @@ const DirectionsSelector = ({ currentType = null, setCurrentType }) => {
           {/* Показываем надпись под активным элементом */}
           {activeItem === id && (
             <div className={stls.mobileAccordeon}>
-              <ProgramList currentType={currentType} ofType={currentType === 'course' ? 'course' : currentType === 'profession' ? 'profession' : null} />
+              <ProgramList
+                currentType={currentType}
+                ofType={
+                  currentType === 'course'
+                    ? 'course'
+                    : currentType === 'profession'
+                    ? 'profession'
+                    : null
+                }
+              />
             </div>
           )}
         </Fragment>
       ))}
     </div>
-  );
-};
+  )
+}
 
-export default DirectionsSelector;
+export default DirectionsSelector

2642693d573b4c7aaaeb43b75203d562539ce5cc
Ivan Kolpakov <vanjaklp95@gmail.com>
2024-03-26 11:16:06 +0300

    psy test on click text added, table styles corrected

--- components/articles/ArticleBlogTable.tsx
+++ components/articles/ArticleBlogTable.tsx
@@ -1,10 +1,5 @@
 import stls from '@/styles/components/articles/ArticleBlogTable.module.sass'
-import Wrapper from '@/components/layout/Wrapper'
-import classNames from 'classnames'
-import marked from 'marked'
-import parse from 'html-react-parser'
-import Image from 'next/image'
-import base64pixel from '@/config/base64pixel'
+
 
 type ArticleBlogTableType = {
   props: {
@@ -25,22 +20,17 @@ const ArticleBlogTable = ({props} : ArticleBlogTableType) => {
 
   return (
       <div style={{backgroundColor: '#FBF4FF'}} className={stls.container}>
-        <table className={stls.table} >
+        <table style={{backgroundColor: '#FBF4FF'}} className={stls.table} >
         <tbody >
           {row.map(oneColumn => (
             
               <tr key={oneColumn.id}>
               {oneColumn.record.map(el => (
-                <td key={el.id} style={{
-                  // border: '1px solid black',
-                  backgroundColor: '#FBF4FF'
-                }} >
+                <td key={el.id} >
                   {el.text}
                 </td>
               ))}
               </tr>
-            
-            //{oneColumn.record.map}
           ))}
           </tbody>
           </table>

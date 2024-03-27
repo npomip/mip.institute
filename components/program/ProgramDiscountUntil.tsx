2bf7e7967ae7137d1855ad1fd494fb957f6892a7
Ivan Kolpakov <vanjaklp95@gmail.com>
2024-03-27 08:12:53 +0300

    dates changed

--- components/program/ProgramDiscountUntil.tsx
+++ components/program/ProgramDiscountUntil.tsx
@@ -3,7 +3,8 @@ import ProgramAdmission from '@/components/program/ProgramAdmission'
 const ProgramDiscountUntil = () => {
   return (
     <>
-      до <ProgramAdmission />
+      {/* до <ProgramAdmission /> */}
+      до 31 марта
     </>
   )
 }
--- helpers/setDateOfEnrollment.ts
+++ helpers/setDateOfEnrollment.ts
@@ -3,32 +3,32 @@ const setDateOfEnrollment = (): string => {
   const selectorDate = item => {
     switch (true) {
       
-      case item < new Date(2024, 2, 6):
-        return new Date(2024, 2, 5).toLocaleString('ru-RU', {
+      // case item < new Date(2024, 3, 1):
+      //   return new Date(2024, 2, 31).toLocaleString('ru-RU', {
+      //     day: 'numeric',
+      //     month: 'long'
+      //   })
+
+      case item < new Date(2024, 3, 3):
+        return new Date(2024, 3, 2).toLocaleString('ru-RU', {
           day: 'numeric',
           month: 'long'
         })
 
-      case item < new Date(2024, 2, 13):
-        return new Date(2024, 2, 12).toLocaleString('ru-RU', {
+      case item < new Date(2024, 3, 10):
+        return new Date(2024, 3, 9).toLocaleString('ru-RU', {
           day: 'numeric',
           month: 'long'
         })
 
-      case item < new Date(2024, 2, 20):
-        return new Date(2024, 2, 19).toLocaleString('ru-RU', {
+      case item < new Date(2024, 2, 17):
+        return new Date(2024, 2, 16).toLocaleString('ru-RU', {
           day: 'numeric',
           month: 'long'
         })
 
-      case item < new Date(2024, 2, 27):
-        return new Date(2024, 2, 26).toLocaleString('ru-RU', {
-          day: 'numeric',
-          month: 'long'
-        })
-
-      case item < new Date(2024, 2, 30):
-        return new Date(2024, 2, 29).toLocaleString('ru-RU', {
+      case item < new Date(2024, 2, 24):
+        return new Date(2024, 2, 23).toLocaleString('ru-RU', {
           day: 'numeric',
           month: 'long'
         })

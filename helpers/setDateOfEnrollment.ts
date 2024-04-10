484eea34c74a9c8731e0f97795162e92f664bc05
Ivan Kolpakov <vanjaklp95@gmail.com>
2024-04-10 10:33:40 +0300

    april dates upd

--- helpers/setDateOfEnrollment.ts
+++ helpers/setDateOfEnrollment.ts
@@ -9,29 +9,24 @@ const setDateOfEnrollment = (): string => {
       //     month: 'long'
       //   })
 
-      case item < new Date(2024, 3, 3):
-        return new Date(2024, 3, 2).toLocaleString('ru-RU', {
+      case item < new Date(2024, 3, 17):
+        return new Date(2024, 3, 16).toLocaleString('ru-RU', {
           day: 'numeric',
           month: 'long'
         })
 
-      case item < new Date(2024, 3, 10):
-        return new Date(2024, 3, 9).toLocaleString('ru-RU', {
+      case item < new Date(2024, 3, 24):
+        return new Date(2024, 3, 23).toLocaleString('ru-RU', {
           day: 'numeric',
           month: 'long'
         })
 
-      case item < new Date(2024, 2, 17):
-        return new Date(2024, 2, 16).toLocaleString('ru-RU', {
+      case item < new Date(2024, 4, 1):
+        return new Date(2024, 3, 30).toLocaleString('ru-RU', {
           day: 'numeric',
           month: 'long'
         })
 
-      case item < new Date(2024, 2, 24):
-        return new Date(2024, 2, 23).toLocaleString('ru-RU', {
-          day: 'numeric',
-          month: 'long'
-        })
     }
   }
 

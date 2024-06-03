a9a60669febce596eabc45b476faffaca026f059
Ivan Kolpakov <vanjaklp95@gmail.com>
2024-06-03 16:52:25 +0300

    place of quiz rearrange

--- pages/index.tsx
+++ pages/index.tsx
@@ -130,11 +130,11 @@ const HomePage: NextPage<TypePageHomeProps> = ({
       </Popup>
       <Hero />
       <Directions />
+      <PsyTest />
       <TopCourses />
       <ButtonToTop />
       <WhyBother />
       <YouTubeVideo videoId='Voc_dKGqhFE' />
-      <PsyTest />
       <About />
       <EducationProcess
         paddingTop={90}

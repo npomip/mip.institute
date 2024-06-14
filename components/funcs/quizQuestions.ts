51524419dea0131f9541a1fde3f132f199d9c569
Ivan Kolpakov <vanjaklp95@gmail.com>
2024-06-14 12:43:32 +0300

    rearrange results

--- components/funcs/quizQuestions.ts
+++ components/funcs/quizQuestions.ts
@@ -3,33 +3,33 @@ const quiz = [
     idx: 1,
     title: 'Что для Вас более предпочтительно?',
     question1: 'Разбираться в глубинных проблемах и сложных случаях',
-    value1: ['organization', 'psySomatic', 'psyCons'],
+    value1: ['clinic', 'psyConsSingle', 'psyCons', 'psySomatic'],
     question2: 'Вдохновлять и мотивировать',
-    value2: ['organization', 'psySomatic'],
+    value2: ['organization', 'psyConsSingle'],
     question3: 'Работать с детско-родительскими отношениями',
-    value3: ['childrenPsy', 'psyCons'],
+    value3: ['childrenPsy', 'psyCons', 'psySomatic', 'psyConsSingle', 'psyAnalisys'],
     question4: 'Помогать в решении жизненных сложностей',
-    value4: ['psyCons', 'psySomatic']
+    value4: ['psyCons','psyConsSingle', 'psySomatic']
   },
   {
     idx: 2,
     title:
       'Вы спринтер (бегун на короткие дистанции) или стайер (бегун на длинные дистанции)?',
     question1: 'Спринтер',
-    value1: ['organization', 'childrenPsy', 'psyCons', 'psySomatic'],
+    value1: ['organization', 'childrenPsy', 'psyConsSingle', 'psySomatic'],
     question2: 'Стайер',
-    value2: [ 'psyCons']
+    value2: [ 'clinic','psyCons', 'psyAnalisys']
   },
   {
     idx: 3,
     title:
       'Какое слово в наибольшей степени характеризует Вас?',
     question1: 'Творческий',
-    value1: ['psyCons', 'psySomatic'],
+    value1: ['psyCons','psyConsSingle', 'psyAnalisys', 'psySomatic'],
     question2: 'Структурный',
-    value2: [],
+    value2: ['clinic', 'psySomatic'],
     question3: 'Дружелюбный',
-    value3: ['childrenPsy', 'psySomatic'],
+    value3: ['childrenPsy','psyConsSingle','organization', 'psySomatic'],
     question4: 'Дипломатичный',
     value4: ['organization', 'psySomatic']
   },
@@ -38,39 +38,39 @@ const quiz = [
     title:
       'Выберите наиболее похожего на Вас персонажа',
     question1: 'Винни-Пух',
-    value1: ['childrenPsy','psySomatic'],
+    value1: ['childrenPsy','psySomatic', 'psyConsSingle'],
     question2: 'Сова',
-    value2: [ 'psyCons', 'psySomatic'],
+    value2: [ 'clinic', 'psyCons','psyAnalisys', 'psySomatic', 'psyConsSingle'],
     question3: 'Кролик',
-    value3: ['organization'],
+    value3: ['organization', 'psySomatic', 'psyConsSingle'],
     question4: 'Пятачок',
-    value4: ['psyCons', 'psySomatic']
+    value4: ['psyCons','psyConsSingle','organization', 'psySomatic']
   },
   {
     idx: 5,
     title:
       'Какие задачи Вам нравится решать?',
       question1: 'Креативные, творческие',
-      value1: ['psyCons', 'psySomatic'],
+      value1: ['psyCons','psyConsSingle', 'psySomatic', 'psyAnalisys'],
       question2: 'Проектные, организационные',
-      value2: ['organization'],
+      value2: ['organization', 'psySomatic'],
       question3: 'Сложные, логические',
-      value3: ['organization', 'psySomatic'],
+      value3: ['clinic', 'psySomatic'],
       question4: 'Развивающие, игровые',
-      value4: ['childrenPsy']
+      value4: ['childrenPsy', 'psyConsSingle']
   },
   {
     idx: 6,
     title:
       'Что Вам ближе?',
     question1: 'Абстрактное, образное',
-    value1: ['psyCons', 'psySomatic'],
+    value1: ['psyConsSingle', 'psyCons', 'psyAnalisys'],
     question2: 'Алгоритмичное, логическое',
-    value2: ['psySomatic'],
+    value2: ['organization', 'psySomatic'],
     question3: 'Телесное, тактильное',
-    value3: ['psySomatic', 'psyCons'],
+    value3: ['psySomatic'],
     question4: 'Эмоциональное, чувственное',
-    value4: ['psyCons']
+    value4: ['psyCons', 'psyConsSingle']
   }
 ]
 

e2429b5aa8fd6f3934c2c947b072e5d447d31cd2
Ivan Kolpakov <vanjaklp95@gmail.com>
2024-07-09 09:39:27 +0300

    props to onClick

--- components/icons/IconCloseCircle.tsx
+++ components/icons/IconCloseCircle.tsx
@@ -4,7 +4,7 @@ type TypeIconCloseCircle = {
   blackCross?: boolean
 }
 
-const IconCloseCircle = ({ blackCross = false, onClick }) => {
+const IconCloseCircle = ({ blackCross = false, onClick=null }) => {
   return (
     <span onClick={onClick} className={stls.container}>
       <svg viewBox='0 0 32 32' fill='none' xmlns='http://www.w3.org/2000/svg'>

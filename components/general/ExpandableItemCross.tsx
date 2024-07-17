1e27733010488aa970fba8f0d1831031e70a0d40
Mikhail Olianenko <mikhail.olianenko@mail.ru>
2024-07-17 18:32:45 +1000

    first cross

--- components/general/ExpandableItemCross.tsx
+++ components/general/ExpandableItemCross.tsx
@@ -1,5 +1,5 @@
 import stls from '@/styles/components/general/ExpandableItemCross.module.sass'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import IconCrossBlue from '../icons/IconCrossBlue'
 
 type Props = {
@@ -11,17 +11,20 @@ type Props = {
 const ExpandableItemCross = ({ title, content, isOpened }: Props) => {
   const [isOpen, setIsOpen] = useState(false)
 
+  useEffect(() => {
+    isOpened && setIsOpen(true)
+  }, [])
+
   return (
     <li className={stls.container}>
       <button className={stls.title} onClick={() => setIsOpen(!isOpen)}>
         <span className={stls.icon}>
-          <IconCrossBlue isRotated={isOpened ? true : isOpen} />
+          <IconCrossBlue isRotated={isOpen} />
         </span>
         <span className={stls.pTitle}>{title}</span>
       </button>
 
-      <div
-        className={`${stls.content} ${isOpen || isOpened ? stls.visible : ''}`}>
+      <div className={`${stls.content} ${isOpen ? stls.visible : ''}`}>
         <div>{content}</div>
       </div>
     </li>

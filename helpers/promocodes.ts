e0f6554035e8647597ceef7227f6c14a7112b8f3
Ivan Kolpakov <vanjaklp95@gmail.com>
2024-07-23 18:36:17 +0300

    fixes with cards, title and promo

--- helpers/promocodes.ts
+++ helpers/promocodes.ts
@@ -2,6 +2,10 @@ const promocodes = {
   pogrebjiskaya: 'Погребижская',
   alex4planet: 'ПЛАНЕТА',
   porakpsihologu: 'Пора к Психологу',
+  muradimovaalina: 'АЛИНА10',
+  maniloun: 'MANILOUN',
+  neprinyato: 'НЕ ПРИНЯТО'
+
 };
 
 export default promocodes

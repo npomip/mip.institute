5bd1e6df1917971d19cdf13da50d473f00fb54f2
Ivan Kolpakov <vanjaklp95@gmail.com>
2024-09-02 14:40:34 +0300

    remove only prod load roistat

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -229,7 +229,7 @@ const MyApp = ({ Component, pageProps, router }) => {
   return (
     <>
       <Script src='https://api.flocktory.com/v2/loader.js?site_id=5428' />
-      {prod && 
+      {/* {prod &&  */}
       <>
       <Script src='/assets/js/vendors/roistatAB.js'/>
       <Script
@@ -246,7 +246,7 @@ const MyApp = ({ Component, pageProps, router }) => {
             }}
           />
       </>
-      }
+      {/* } */}
       
       <DefaultSeo {...SEO} />
       <div style={{display: 'none'}}>

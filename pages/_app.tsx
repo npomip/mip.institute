ba7230ae36126a069b69cc80a8c7dd235cf33977
Ivan Kolpakov <vanjaklp95@gmail.com>
2024-09-02 14:58:45 +0300

    change strategy of roistat script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -230,22 +230,23 @@ const MyApp = ({ Component, pageProps, router }) => {
     <>
       <Script src='https://api.flocktory.com/v2/loader.js?site_id=5428' />
       {/* {prod &&  */}
-      <>
-      <Script src='/assets/js/vendors/roistatAB.js'/>
+      {/* <> */}
+      <Script strategy='beforeInteractive' src='/assets/js/vendors/roistatAB.js'/>
       <Script
+      strategy='beforeInteractive'
             id='roistat counter'
             dangerouslySetInnerHTML={{
               __html: `
             (function(w, d, s, h, id) {
-              w.roistatProjectId = id; w.roistatHost = h;
-              var p = d.location.protocol == "https:" ? "https://" : "http://";
-              var u = /^.*roistat_visit=[^;]+(.*)?$/.test(d.cookie) ? "/dist/module.js" : "/api/site/1.0/"+id+"/init?referrer="+encodeURIComponent(d.location.href);
-              var js = d.createElement(s); js.charset="UTF-8"; js.async = 1; js.src = p+h+u; var js2 = d.getElementsByTagName(s)[0]; js2.parentNode.insertBefore(js, js2);
-            })(window, document, 'script', 'cloud.roistat.com', '5504efcdd803f95c53cf52800d65f41b');
+    w.roistatProjectId = id; w.roistatHost = h;
+    var p = d.location.protocol == "https:" ? "https://" : "http://";
+    var u = /^.*roistat_visit=[^;]+(.*)?$/.test(d.cookie) ? "/dist/module.js" : "/api/site/1.0/"+id+"/init?referrer="+encodeURIComponent(d.location.href);
+    var js = d.createElement(s); js.charset="UTF-8"; js.async = 1; js.src = p+h+u; var js2 = d.getElementsByTagName(s)[0]; js2.parentNode.insertBefore(js, js2);
+})(window, document, 'script', 'cloud.roistat.com', '5504efcdd803f95c53cf52800d65f41b');
           `
             }}
           />
-      </>
+      {/* </> */}
       {/* } */}
       
       <DefaultSeo {...SEO} />

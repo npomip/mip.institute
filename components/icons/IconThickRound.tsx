55179365d1c9f68c053724200f7068dc99ca7494
Mikhail Olianenko <mikhail.olianenko@mail.ru>
2024-09-10 23:01:07 +1000

    описание

--- components/icons/IconThickRound.tsx
+++ components/icons/IconThickRound.tsx
@@ -1,6 +1,9 @@
-import stls from '@/styles/components/icons/IconThickRedRound.module.sass'
+import stls from '@/styles/components/icons/IconThickRound.module.sass'
 
-const IconThickRedRound = () => {
+type Props = {
+  fill?: string
+}
+const IconThickRound = ({ fill = '#F24E1E' }: Props) => {
   return (
     <span className={stls.container}>
       <svg
@@ -9,7 +12,7 @@ const IconThickRedRound = () => {
         height='20'
         viewBox='0 0 20 20'
         fill='none'>
-        <rect width='20' height='20' rx='10' fill='#F24E1E' />
+        <rect width='20' height='20' rx='10' fill={fill} />
         <path
           d='M13.3158 8L9.10526 12.2106L7 10.1053'
           stroke='white'
@@ -22,4 +25,4 @@ const IconThickRedRound = () => {
   )
 }
 
-export default IconThickRedRound
+export default IconThickRound

7e88054c67f08ae3c954414b8d175553b7dfd47d
Egor Smelov <smelov909@mail.ru>
2024-10-02 16:37:46 +0300

    new diplomas and head

--- components/imgs/diplomas/ImgDiploma.tsx
+++ components/imgs/diplomas/ImgDiploma.tsx
@@ -1,6 +1,6 @@
 import stls from '@/styles/components/imgs/diplomas/ImgDiploma.module.sass'
 import Image from 'next/image'
-import pic from '@/public/assets/imgs/diplomas/diploma.jpeg'
+import pic from '@/public/assets/imgs/diplomas/diplomaNew.jpg'
 
 const ImgDiploma = ({ width = null, height = null }) => {
   return (

8bd8c94ab6057508f8edcfb96e9b2dbb538d2b96
Egor Smelov <smelov909@mail.ru>
2024-10-17 13:29:27 +0300

    add LectoriumCertificate

--- pages/lectorium/[slug].tsx
+++ pages/lectorium/[slug].tsx
@@ -1,5 +1,6 @@
 import DownloadProgram from '@/components/sections/lectorium/DownloadProgram'
 import HaveQuestions from '@/components/sections/lectorium/HaveQuestions'
+import LectoriumCertificate from '@/components/sections/lectorium/LectoriumCertificate'
 import VideoReviews from '@/components/sections/lectorium/VideoReviews'
 import { NextSeo } from 'next-seo'
 
@@ -8,6 +9,7 @@ const LectoriumPage = () => {
     <>
       <NextSeo nofollow={true} noindex={true} />
       <DownloadProgram />
+      <LectoriumCertificate />
       <VideoReviews />
       <HaveQuestions />
     </>

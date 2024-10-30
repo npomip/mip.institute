4a9ea69701f202bf7a1d3de1969eedc507e6a429
Mikhail Olianenko <mikhail.olianenko@mail.ru>
2024-10-30 21:02:27 +1100

    заголовки

--- components/sections/practicalTraining/PracticalHeroProgram.tsx
+++ components/sections/practicalTraining/PracticalHeroProgram.tsx
@@ -46,58 +46,64 @@ const PracticalHeroProgram = ({ breadcrumbs, practicalTraining }: Props) => {
   return (
     <>
       {isMobileAndTabletLayout && (
-        <div
-          className={stls.mobileBg}
-          style={{
-            backgroundImage: `url(${practicalTraining?.heroPicture?.url})`
-          }}>
-          <span className={stls.filter}></span>
-          <div className={stls.content}>
-            <div>
-              {tag}
-              <h1 className={stls.title}>{title}</h1>
-              <div className={stls.mobileFlex}>
-                <div className={stls.descriptionMobile}>
-                  <p className={stls.mobiledesc}>
-                    {practicalTraining?.subtitle}
-                  </p>
-                </div>
-              </div>
-              <div className={stls.btnsMobile}>
-                <PopupTrigger btn='alpha' cta={cta} />
-                <PopupTrigger btn='beta' cta='askQuestion' />
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
-      <div className={stls.info}>
-        <PracticalProgramInfo practicalTraining={practicalTraining} />
-      </div>
-      <section className={stls.container}>
-        <Wrapper>
+        <>
           <div
-            className={stls.desktopBg}
+            className={stls.mobileBg}
             style={{
               backgroundImage: `url(${practicalTraining?.heroPicture?.url})`
             }}>
             <span className={stls.filter}></span>
-            <div className={stls.heading}>
-              {/* <Breadcrumbs breadcrumbs={breadcrumbs} /> */}
-              {tag}
-              <h1 className={stls.title}>{title}</h1>
-              <div className={stls.descriptionDesktop}>
-                <p className={stls.mobiledesc}>{practicalTraining?.subtitle}</p>
+            <div className={stls.content}>
+              <div>
+                {tag}
+                <h1 className={stls.title}>{title}</h1>
+                <div className={stls.mobileFlex}>
+                  <div className={stls.descriptionMobile}>
+                    <p className={stls.mobiledesc}>
+                      {practicalTraining?.subtitle}
+                    </p>
+                  </div>
+                </div>
+                <div className={stls.btnsMobile}>
+                  <PopupTrigger btn='alpha' cta={cta} />
+                  <PopupTrigger btn='beta' cta='askQuestion' />
+                </div>
               </div>
-              <div className={stls.btnsDesktop}>
-                <PopupTrigger btn='alpha' cta='signUpForCourse' />
-                <PopupTrigger btn='beta' cta='askQuestion' />
-              </div>
-              <PracticalProgramInfo practicalTraining={practicalTraining} />
             </div>
           </div>
-        </Wrapper>
-      </section>
+          <div className={stls.info}>
+            <PracticalProgramInfo practicalTraining={practicalTraining} />
+          </div>
+        </>
+      )}
+      {!isMobileAndTabletLayout && (
+        <section className={stls.container}>
+          <Wrapper>
+            <div
+              className={stls.desktopBg}
+              style={{
+                backgroundImage: `url(${practicalTraining?.heroPicture?.url})`
+              }}>
+              <span className={stls.filter}></span>
+              <div className={stls.heading}>
+                {/* <Breadcrumbs breadcrumbs={breadcrumbs} /> */}
+                {tag}
+                <h1 className={stls.title}>{title}</h1>
+                <div className={stls.descriptionDesktop}>
+                  <p className={stls.mobiledesc}>
+                    {practicalTraining?.subtitle}
+                  </p>
+                </div>
+                <div className={stls.btnsDesktop}>
+                  <PopupTrigger btn='alpha' cta='signUpForCourse' />
+                  <PopupTrigger btn='beta' cta='askQuestion' />
+                </div>
+                <PracticalProgramInfo practicalTraining={practicalTraining} />
+              </div>
+            </div>
+          </Wrapper>
+        </section>
+      )}
     </>
   )
 }

bd365b7aa016430a9889198681c4090583c228b0
Egor Smelov <smelov909@mail.ru>
2024-11-26 13:17:06 +0300

    edit

--- components/program/ProgramDiscount.tsx
+++ components/program/ProgramDiscount.tsx
@@ -4,6 +4,9 @@ import classNames from 'classnames'
 import { ContextStaticProps } from '@/context/index'
 import { useContext } from 'react'
 import ProgramDiscountUntil from './ProgramDiscountUntil'
+import Popup from 'reactjs-popup'
+import IconInfoOrange from '../icons/IconInfoOrange'
+import useBetterMediaQuery from '@/hooks/general/UseBetterMediaQuery'
 
 type Props = {
   small?: boolean
@@ -20,8 +23,11 @@ const ProgramDiscount = ({
 }: Props) => {
   const { program } = useContext(ContextStaticProps)
   const programDiscount = program?.discount || 0
+  const isMobileAndTabletLayout = useBetterMediaQuery('(max-width: 768px)')
 
-  const elDiscount = <>{programDiscount ? `-${programDiscount}%` : discount}</>
+  const elDiscount = (
+    <>{programDiscount ? `до -${programDiscount}%` : discount}</>
+  )
 
   return (
     <>
@@ -37,7 +43,29 @@ const ProgramDiscount = ({
           })}>
           {violet ? (
             <div className={stls.forFlex}>
-              <span className={stls.round}>●</span>
+              <Popup
+                trigger={
+                  <button
+                    className={stls.info}
+                    aria-label='Информация о скидках'>
+                    <IconInfoOrange />
+                  </button>
+                }
+                position='top left'
+                on={isMobileAndTabletLayout ? 'click' : 'hover'}
+                closeOnDocumentClick
+                arrow={false}
+                contentStyle={{
+                  boxShadow: 'none'
+                }}>
+                <div className={stls.tooltip}>
+                  <div className={stls.icon}>
+                    <IconInfoOrange />
+                  </div>
+                  Информацию о скидках и дополнительных бонусах при поступлении
+                  уточняйте у менеджеров приемной комиссии
+                </div>
+              </Popup>
               <div className={stls.rightPart}>
                 <p className={stls.discount}>{elDiscount}</p>
                 <p className={stls.until}>

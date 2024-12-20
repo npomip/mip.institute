5912079082cb3fd850fab05657c9883bbae570b1
dv.udanov <dv.udanov@tensor.ru>
2024-12-20 18:31:05 +0300

    create Star background

--- components/sections/App/StickyButtonNewYers/StickyButtonNewYers.tsx
+++ components/sections/App/StickyButtonNewYers/StickyButtonNewYers.tsx
@@ -35,15 +35,15 @@ const StickyBottomNewYers = () => {
           [stls.container]: true,
           [stls.isShown]: isShown,
           [stls.isClosed]: isClosed
-        })}
-      >
+        })}>
+        <div className={stls.fullBackground}></div>
+
         <div className={stls.shape}>
           <Image
-            alt="cytu"
-            src="https://res.cloudinary.com/mipinstitute/image/upload/v1734687978/Snezhinki_f1dbb83e5c.png"
+            alt='cytu'
+            src='https://res.cloudinary.com/mipinstitute/image/upload/v1734687978/Snezhinki_f1dbb83e5c.png'
             width={250}
             height={95}
-
           />
         </div>
         <Wrapper>
@@ -51,18 +51,19 @@ const StickyBottomNewYers = () => {
             <div className={stls.text}>
               <Popup
                 trigger={
-                  <button className={stls.info} aria-label="Информация о скидках">
+                  <button
+                    className={stls.info}
+                    aria-label='Информация о скидках'>
                     <IconInfoOrange />
                   </button>
                 }
-                position="top left"
+                position='top left'
                 on={window.innerWidth <= 768 ? 'click' : 'hover'}
                 closeOnDocumentClick
                 arrow={false}
                 contentStyle={{
                   boxShadow: 'none'
-                }}
-              >
+                }}>
                 <div className={stls.tooltip}>
                   <div className={stls.icon}>
                     <IconInfoOrange />
@@ -74,7 +75,9 @@ const StickyBottomNewYers = () => {
               <span className={stls.title}>
                 Скидка до <span className={stls.orangeTitle}>{discount}</span>
               </span>
-              <span className={stls.description}>на все программы и курсы </span>
+              <span className={stls.description}>
+                на все программы и курсы{' '}
+              </span>
               <span className={stls.discount}>
                 <ProgramDiscountUntil />
               </span>
@@ -83,55 +86,57 @@ const StickyBottomNewYers = () => {
           <div className={stls.containerGift}>
             <span className={stls.tree}>
               <Image
-                alt="cytu"
-                src="https://res.cloudinary.com/mipinstitute/image/upload/v1734695043/Bolshaya_Yolka_de4ce98ba9.png"
+                alt='cytu'
+                src='https://res.cloudinary.com/mipinstitute/image/upload/v1734695043/Bolshaya_Yolka_de4ce98ba9.png'
                 width={60}
                 height={70}
               />
             </span>
             <span className={stls.gift}>
               <Image
-                alt="cytu"
-                src="https://res.cloudinary.com/mipinstitute/image/upload/v1734695218/Podarok_c0298bce6e.png"
+                alt='cytu'
+                src='https://res.cloudinary.com/mipinstitute/image/upload/v1734695218/Podarok_c0298bce6e.png'
                 width={55}
                 height={40}
               />
             </span>
             <span className={stls.ballTree}>
               <Image
-                alt="cytu"
-                src="https://res.cloudinary.com/mipinstitute/image/upload/v1734695245/Malenkaya_yolka_de4e4a9a44.png"
+                alt='cytu'
+                src='https://res.cloudinary.com/mipinstitute/image/upload/v1734695245/Malenkaya_yolka_de4e4a9a44.png'
                 width={27}
                 height={24}
               />
             </span>
           </div>
           <div className={stls.btns}>
-            <PopupTrigger btn="alpha" cta="submitApplication" />
+            <PopupTrigger btn='alpha' cta='submitApplication' />
             <div className={stls.btn2}>
-              <PopupTrigger btn="beta" cta="consultMe" />
+              <PopupTrigger btn='beta' cta='consultMe' />
             </div>
             <div className={stls.bow}>
               <Image
-                alt="cytu"
-                src="https://res.cloudinary.com/mipinstitute/image/upload/v1734689519/Novogodnij_Bant_f0c5361c74.png"
+                alt='cytu'
+                src='https://res.cloudinary.com/mipinstitute/image/upload/v1734689519/Novogodnij_Bant_f0c5361c74.png'
                 width={88}
                 height={77}
               />
             </div>
           </div>
-          <div className="js-whatsapp-message-container" style={{ display: 'none' }}>
+          <div
+            className='js-whatsapp-message-container'
+            style={{ display: 'none' }}>
             <p>
-              Обязательно отправьте это сообщение и дождитесь ответа. Ваш номер обращения:{' '}
-              {roistat_visit}
+              Обязательно отправьте это сообщение и дождитесь ответа. Ваш номер
+              обращения: {roistat_visit}
             </p>
           </div>
           <div className={stls.btnMobile}>
-            <PopupTrigger btn="alpha" cta="submitApplication" />
+            <PopupTrigger btn='alpha' cta='submitApplication' />
             <div className={stls.bow}>
               <Image
-                alt="cytu"
-                src="https://res.cloudinary.com/mipinstitute/image/upload/v1734689519/Novogodnij_Bant_f0c5361c74.png"
+                alt='cytu'
+                src='https://res.cloudinary.com/mipinstitute/image/upload/v1734689519/Novogodnij_Bant_f0c5361c74.png'
                 width={88}
                 height={77}
               />

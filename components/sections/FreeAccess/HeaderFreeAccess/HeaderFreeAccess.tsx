5674955c22937609c768ea04532208ba86bd1b13
Ivan Kolpakov <vanjaklp95@gmail.com>
2024-12-24 14:37:20 +0300

    upd header img

--- components/sections/FreeAccess/HeaderFreeAccess/HeaderFreeAccess.tsx
+++ components/sections/FreeAccess/HeaderFreeAccess/HeaderFreeAccess.tsx
@@ -1,39 +1,15 @@
 import React from 'react'
 import styles from './HeaderFreeAccess.module.sass'
-import classNames from 'classnames'
-import Image from 'next/image'
 
 const HeaderFreeAcces = () => {
   return (
-    <section>
-      <div className={styles.container}>
-        <div className={styles.imageWrapper}>
-          <Image
-            src={
-              'https://res.cloudinary.com/mipinstitute/image/upload/v1734966269/Banner_Besplatnogo_Dostupa_22431f0eb7.png'
-            }
-            alt={'Новогодний баннер'}
-            width={'1159'}
-            height={'559'}
-            style={{ width: '100%', height: 'auto' }}
-          />
-        </div>
-        <div className={styles.bannerContent}>
-          <span className={styles.title}>
-            ПОЛУЧИТЕ <span className={styles.violetTitle}>БЕСПЛАТНЫЙ </span>
-          </span>
-          <br />
-          <span className={classNames(styles.title, styles.violetTitle)}>
-            ПЕРСОНАЛЬНЫЙ ДОСТУП
-            <br />
-          </span>
-          <span className={styles.title}>
-            ко всем вебинарам Московского Института Психологии по разным
-            направлениям
-          </span>
-        </div>
-      </div>
-    </section>
+    <div className={styles.bannerContent}>
+      <p>
+        Получите <span>бесплатный персональный доступ</span>
+        <br /> ко всем вебинарам Московского Института Психологии по разным
+        направлениям
+      </p>
+    </div>
   )
 }
 

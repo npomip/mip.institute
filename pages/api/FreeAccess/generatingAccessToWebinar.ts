dbf3b2e97de7ba6d0e3c865169e45e5a19e7c237
dv.udanov <dv.udanov@tensor.ru>
2024-12-25 17:44:13 +0300

    добавил параметр в ссылку

--- pages/api/FreeAccess/generatingAccessToWebinar.ts
+++ pages/api/FreeAccess/generatingAccessToWebinar.ts
@@ -11,8 +11,8 @@ import crypto from 'crypto'
 //     // g=название группы в которую нужно зачислить слушателя, если группы нет в системе она будет создана.
 //     // t=время отправки запроса в формате unixtime (не может отличаться более чем на минуту от времени на сервере)
 //     // k=разовый ключ, который должын быть свормирован как md5($kk.$un.$t), где $kk статический ключ равный 3ykOQzkL2X647dWw8dDx7h5c, $un - имя пользователя, $t - время отправки запроса.
-//      ee (от end education) - окончание обучения 
-//      be (от begin education) – начало в формате unixtime 
+//      ee (от end education) - окончание обучения
+//      be (от begin education) – начало в формате unixtime
 
 //     // https://lms.mip.institute/local/ilogin/rlogin.php?un=Иван&pw=bcbc1aa1-e162-42e1-b6ce-9c5bcd285ea5&ln=Kolpakov&fn=Ivan&g=87&e=vanjaklp@yandex.ru&t=1722432676&k=5effc26772817aeba1642a051fbb5cde,
 
@@ -36,11 +36,12 @@ export default async function handler(req, res) {
       const time = Math.floor(Date.now() / 1000)
       const keyString = `${staticKey}${email}${time}`
       const k = crypto.createHash('md5').update(keyString).digest('hex')
-      const endEducation = time + 259200 
+      const endEducation = time + 259200
       const beginEducation = time
+      const g = ЗнакомимсясМИП
 
       // Создание ссылки для логина
-      const Link = `https://lms.mip.institute/local/ilogin/rlogin.php?un=${email}&pw=${generatePswrd}&ln=${lastName}&fn=${firstName}&g=87&e=${email}&t=${time}&k=${k}&ee=${endEducation}&be=${beginEducation}`
+      const Link = `https://lms.mip.institute/local/ilogin/rlogin.php?un=${email}&pw=${generatePswrd}&ln=${lastName}&fn=${firstName}&g=87&e=${email}&t=${time}&k=${k}&ee=${endEducation}&be=${beginEducation}&g=${g}`
 
       res
         .status(200)

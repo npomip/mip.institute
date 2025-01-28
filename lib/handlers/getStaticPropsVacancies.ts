380809cb44e8d1116cddbe7a1fe26efc971d949d
Ivan Kolpakov <vanjaklp95@gmail.com>
2025-01-28 14:38:34 +0300

    upd props for recruitment

--- lib/handlers/getStaticPropsVacancies.ts
+++ lib/handlers/getStaticPropsVacancies.ts
@@ -1,9 +1,7 @@
-import revalidate from "@/config/revalidate"
-import routes from "@/config/routes"
-import axios from "axios"
-import qs from "qs"
-
-// test token strapi v 5 
+import revalidate from '@/config/revalidate'
+import routes from '@/config/routes'
+import axios from 'axios'
+import qs from 'qs'
 
 const queryString = qs.stringify(
   {
@@ -11,11 +9,47 @@ const queryString = qs.stringify(
       hero: {
         populate: '*'
       },
-      blocks: {
+      seo: {
         populate: '*'
       },
-    },
-    // fields: ['id', 'title', 'slug', 'subtitle', 'studyField', 'studyFieldSlug', 'date', 'previewOnly']
+      blocks: {
+        populate: '*',
+        on: {
+          'vacancies.repeatable-quote-with-title': {
+            populate: '*'
+          },
+          'shared.text-with-icon': {
+            populate: '*'
+          },
+          'vacancies.slider-with-image': {
+            populate: '*'
+          },
+          'shared.rich-text': {
+            populate: '*'
+          },
+          'shared.rich-text-with-img': {
+            populate: '*'
+          },
+          'vacancies.slider-with-img': {
+            populate: '*'
+          },
+          'vacancies.recruitment': {
+            populate: {
+              recruiter: {
+                populate: {
+                  image: {
+                    fields: ['url', 'width', 'height']
+                  }
+                }
+              }
+            }
+          },
+          'blog.list-with-bg-and-title': {
+            populate: '*'
+          }
+        }
+      }
+    }
   },
   {
     encodeValuesOnly: true,
@@ -23,21 +57,19 @@ const queryString = qs.stringify(
   }
 )
 export const getStaticPropsVacancies = async () => {
-try {
-  const response = await axios.get(`${routes.back.rootv2}/api/vacancy?${queryString}`, {
-    headers: {
-      'Authorization': `Bearer ${process.env.STRAPI_BEARER}`, // Замените на ваш токен
-    },
-  })
-
-  return {
-    props: {
-      vacancies: response?.data?.data || []
-    },
-    revalidate: revalidate.default
+  try {
+    const response = await axios.get(`${routes.back.rootv2}/api/vacancy?${queryString}`, {
+      headers: {
+        Authorization: `Bearer ${process.env.STRAPI_BEARER}` // Замените на ваш токен
+      }
+    })
+    return {
+      props: {
+        vacancies: response?.data?.data || []
+      },
+      revalidate: revalidate.default
+    }
+  } catch (e) {
+    console.log(e)
   }
-}catch(e){
-  console.log(e)}
-
 }
-

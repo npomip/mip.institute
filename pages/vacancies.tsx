acdbd8109f4bc3664aa4610b58422c6cc54d6a95
Ivan Kolpakov <vanjaklp95@gmail.com>
2025-02-07 17:30:51 +0300

    upd seo vacancies

--- pages/vacancies.tsx
+++ pages/vacancies.tsx
@@ -1,9 +1,19 @@
 import PageVacancies from '@/components/pages/PageVacancies'
+import SeoCommon from '@/components/seo/SeoCommon'
 import { getStaticPropsVacancies } from '@/lib/handlers/getStaticPropsVacancies'
 import { GetStaticProps } from 'next'
 
 const VacanciesIndexPage = ({ vacancies }) => {
-  return <PageVacancies vacancies={vacancies} />
+  console.log(vacancies.seo);
+  
+  return (
+    <>
+    <SeoCommon seo={vacancies.seo[0]} />
+    <PageVacancies vacancies={vacancies} />
+
+    </>
+
+)
 }
 
 export const getStaticProps: GetStaticProps = async () => await getStaticPropsVacancies()

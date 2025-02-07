1f177050b253451c9d1ffbbf050edd54e11a30a2
Ivan Kolpakov <vanjaklp95@gmail.com>
2025-02-07 16:43:26 +0300

    finish with props

--- lib/handlers/getStaticPropsIncomers.ts
+++ lib/handlers/getStaticPropsIncomers.ts
@@ -3,9 +3,118 @@ import routes from '@/config/routes'
 import axios from 'axios'
 import qs from 'qs'
 
+// const queryString = qs.stringify(
+//   {
+//     populate: {
+//       hero: {
+//         populate: '*'
+//       },
+//       seo: {
+//         populate: '*'
+//       },
+//       blocks: {
+//         populate: '*',
+//         on: {
+//           'vacancies.repeatable-quote-with-title': {
+//             populate: '*'
+//           },
+//           'shared.text-with-icon': {
+//             populate: '*'
+//           },
+//           'vacancies.slider-with-image': {
+//             populate: {
+              
+//               slider: {
+//                 populate: '*'
+//               },
+//               img: {
+//                 populate: '*'
+//               }
+
+//             }
+//           },
+//           'shared.rich-text': {
+//             populate: '*'
+//           },
+//           'shared.rich-text-with-img': {
+//             populate: '*'
+//           },
+//           'vacancies.slider-with-img': {
+//             populate: '*'
+//           },
+//           'vacancies.recruitment': {
+//             populate: {
+//               recruiter: {
+//                 populate: {
+//                   image: {
+//                     fields: ['url', 'width', 'height']
+//                   }
+//                 }
+//               }
+//             }
+//           },
+//           'blog.list-with-bg-and-title': {
+//             populate: '*'
+//           }
+//         }
+//       }
+//     }
+//   },
+//   {
+//     encodeValuesOnly: true,
+//     skipNulls: true
+//   }
+// )
+
 const queryString = qs.stringify(
   {
-    populate: '*'
+    populate: {
+      incomersInfo: {
+        populate: {
+          img :{
+            fields: ['url', 'width', 'height']
+          }
+        },
+
+      },
+      AdventureCards: {
+        populate: {
+          carousel: {
+            populate: {
+              slide: {
+                populate: {
+                  files: {
+                    fields: ['url', 'width', 'height']
+                  }
+                }
+              }
+            }
+          }
+        }
+      },
+      MeetInstitute: {
+        populate: '*'
+      },
+      studyProcess: {
+        populate: '*'
+      },
+      programSelectionsTop: {
+        populate: '*'
+      },
+      programSelectionsBottom: {
+        populate: '*'
+      },
+      ourPossibilities: {
+        populate: '*'
+      },
+      careerCenter: {
+        populate: {
+          img :{
+            fields: ['url', 'width', 'height']
+          }
+        }
+      },
+    }
   },
   {
     encodeValuesOnly: true,
@@ -15,7 +124,7 @@ const queryString = qs.stringify(
 export const getStaticPropsIncomers = async () => {
   try {
     
-    const response = await axios.get(`${routes.back.rootv2}/api/incomer`, {
+    const response = await axios.get(`${routes.back.rootv2}/api/incomer?${queryString}`, {
       headers: {
         Authorization: `Bearer ${process.env.STRAPI_BEARER}` // Замените на ваш токен
       }

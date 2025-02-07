515c5ac0324ee895abaf39cbbdb53064b41a68ba
Ivan Kolpakov <vanjaklp95@gmail.com>
2025-02-07 13:41:54 +0300

    upd 1000 promo

--- helpers/promoWithGIfts.ts
+++ helpers/promoWithGIfts.ts
@@ -8,6 +8,7 @@ const promocodesWithGift = {
   sobchak0702: { gift: 'https://mipinstitute.getcourse.ru/3shaga_dostup' },
   vasilisavolodinaofficial: { gift: 'https://mipinstitute.getcourse.ru/3shaga_dostup' },
   dorogedeneg : { gift: 'https://mipinstitute.getcourse.ru/3shaga_dostup' },
+  '1000idey2': { gift: 'https://mipinstitute.getcourse.ru/3shaga_dostup' },
 }
 
 export default promocodesWithGift
